26afcf1713cd927b3689fab582cd3850cca5d8dc
TeBeCo <TeBeCo@gmail.com>
2025-08-03 20:33:12 +0400

    adding Todo.cs

---
+++ src/TodoJsAspire.ApiService/Db/Todo.cs
@@ -0,0 +1,15 @@
+namespace TodoJsAspire.ApiService.Db;
+
+public class Todo
+{
+    public required int Id { get; set; }
+
+    public required string Title { get; set; }
+
+    public bool IsComplete { get; set; } = false;
+
+    // The position of the todo in the list, used for ordering.
+    // When updating this, make sure to not duplicate values.
+    // To move an item up/down, swap the values of the position
+    public required int Position { get; set; }
+}
