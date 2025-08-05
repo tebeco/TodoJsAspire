b6b5edf8b8d9fd4d0c14892893dbb373fcb707d7
TeBeCo <TeBeCo@gmail.com>
2025-08-05 12:27:00 +0400

    rest of generated "dotnet dnx scaffold"

--- src/TodoJsAspire.ApiService/Program.cs
+++ src/TodoJsAspire.ApiService/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using TodoJsAspire.ApiService.Db;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("TodoDbContext") ?? throw new InvalidOperationException("Connection string 'TodoDbContext' not found.");
+
+builder.Services.AddDbContext<TodoDbContext>(options => options.UseNpgsql(connectionString));
 
 // Add service defaults & Aspire client integrations.
 builder.AddServiceDefaults();
@@ -19,27 +25,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-string[] summaries = ["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.MapDefaultEndpoints();
+app.MapTodoEndpoints();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
