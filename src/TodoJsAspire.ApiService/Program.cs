345330a0350d0aab99e3fb9a2bd2b75739bd33fa
TeBeCo <TeBeCo@gmail.com>
2025-08-09 02:47:57 +0400

    add scalar UI

--- src/TodoJsAspire.ApiService/Program.cs
+++ src/TodoJsAspire.ApiService/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 using TodoJsAspire.ApiService.Db;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -32,6 +33,7 @@ app.UseExceptionHandler();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.MapDefaultEndpoints();
