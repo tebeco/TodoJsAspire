05358e1000d053d8facbd4986a55d9f013b847ed
TeBeCo <TeBeCo@gmail.com>
2025-08-09 02:47:44 +0400

    adapt vite config to Aspire

--- todo-frontend/vite.config.ts
+++ todo-frontend/vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({mode}) => {
+    const env = loadEnv(mode, process.cwd(), '')
+
+    return {
+        plugins: [react()],
+        server: {
+            port: parseInt(env.VITE_PORT),
+            proxy: {
+                // "apiservice" is the name of the API in AppHost.cs.
+                '/api': {
+                    target: process.env.services__apiservice__https__0 || process.env.services__apiservice__http__0,
+                    changeOrigin: true,
+                    secure: false,
+                    rewrite: (path) => path.replace(/^\/api/, '/api')
+                }
+            }
+        },
+        build:{
+            outDir: 'dist',
+            rollupOptions: {
+                input: './index.html'
+            }
+        }
+    }
 })
